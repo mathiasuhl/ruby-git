d4f3c95e0a5d29ccde0b2b44158a120c36b758fc
Mathias Uhl <m.uhl@ibdigital.de>
2013-11-16 02:20:30 +0100

    change max log to a high number cos we want to have a version for a file over 30 :)

--- lib/git/log.rb
+++ lib/git/log.rb
@@ -4,7 +4,7 @@ module Git
   class Log
     include Enumerable
     
-    def initialize(base, count = 30)
+    def initialize(base, count = 99999999999)
       dirty_log
       @base = base
       @count = count
